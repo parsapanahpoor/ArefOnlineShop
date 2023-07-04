6c7d7d424cfe44bdde082236b802b79b22f15a19
parsa panahpoor <parsapanahpoor77@outlook.com>
2023-07-05 00:42:20 +0330

    after update database

---
+++ Data/Migrations/20230704211156_Add-ImegName-For-ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddImegNameForProductCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "ProductCategories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "ProductCategories");
+        }
+    }
+}
