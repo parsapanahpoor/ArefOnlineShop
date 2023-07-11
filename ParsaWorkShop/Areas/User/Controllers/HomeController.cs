043097154c85511b1832a28c555f0baff70a2af6
parsa panahpoor <parsapanahpoor77@outlook.com>
2023-07-11 15:05:51 +0330

    start of user panel

--- ParsaWorkShop/Areas/User/Controllers/HomeController.cs
+++ ParsaWorkShop/Areas/User/Controllers/HomeController.cs
@@ -1,31 +1,35 @@
-﻿using Application.Interfaces;
-using Domain.Models.Users;
+﻿#region Using
+
+using Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+#endregion
 
 namespace ParsaWorkShop.Areas.User.Controllers
 {
-    [Area("User")]
-    [Authorize]
-    public class HomeController : Controller
+    public class HomeController : UserPanelBaseController
     {
         #region Ctor
+
         private IUserService _userservice;
         public HomeController(IUserService userService)
         {
             _userservice = userService;
         }
+
         #endregion
 
+        #region Index
 
         public IActionResult Index()
         {
             var user = _userservice.GetUserByUserName(User.Identity.Name);
+
             return View(user);
         }
+
+        #endregion
+
     }
 }
---
+++ ParsaWorkShop/Areas/User/ViewComponents/UserPanelSideBarViewComponents.cs
@@ -0,0 +1,26 @@
+﻿using Application.Extensions;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ParsaWorkShop.Areas.User.ViewComponents
+{
+    public class UserPanelSideBarViewComponents : ViewComponent
+    {
+        #region Ctor
+
+        private readonly IUserService _userService;
+
+        public UserPanelSideBarViewComponents(IUserService userService)
+        {
+                _userService = userService;
+        }
+
+        #endregion
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("UserPanelSideBar" , await _userService.GetUserByIdAsync(User.GetUserId()));
+        }
+    }
+}
