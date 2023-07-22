27035ab9d961f586e246225e764a16fc05956bb9
parsa panahpoor <parsapanahpoor77@outlook.com>
2023-07-22 14:41:43 +0330

    mohandes sadeghi

--- ParsaWorkShop/wwwroot/ArefSet/src/JS/productSinglePage.js
+++ ParsaWorkShop/wwwroot/ArefSet/src/JS/productSinglePage.js
@@ -18,81 +18,81 @@ btnRemoveCounter.addEventListener("click", function () {
 });
 
 // -------------------------------------------------
-const imgContElm = document.querySelector(".img-container");
-const imgElm = document.querySelector(".img-container img");
-const listProductElm = document.querySelector(".list-product");
+//const imgContElm = document.querySelector(".img-container");
+//const imgElm = document.querySelector(".img-container img");
+//const listProductElm = document.querySelector(".list-product");
 
-// precentage of the zoom
-const ZOOM = 300;
+//// precentage of the zoom
+//const ZOOM = 300;
 
-//Event Mouse Enter
-imgContElm.addEventListener("mouseenter", function () {
-  imgElm.style.width = ZOOM + "%";
-});
+////Event Mouse Enter
+//imgContElm.addEventListener("mouseenter", function () {
+//  imgElm.style.width = ZOOM + "%";
+//});
 
-//Event Mouse Leave
-imgContElm.addEventListener("mouseleave", function () {
-  imgElm.style.width = "100%";
-  imgElm.style.top = "0";
-  imgElm.style.left = "0";
-});
+////Event Mouse Leave
+//imgContElm.addEventListener("mouseleave", function () {
+//  imgElm.style.width = "100%";
+//  imgElm.style.top = "0";
+//  imgElm.style.left = "0";
+//});
 
-//Event Mouse Move:
-imgContElm.addEventListener("mousemove", function (mouseEvent) {
-  let obj = imgContElm;
-  let obj_left = 0;
-  let obj_top = 0;
-  let xpos;
-  let ypos;
+////Event Mouse Move:
+//imgContElm.addEventListener("mousemove", function (mouseEvent) {
+//  let obj = imgContElm;
+//  let obj_left = 0;
+//  let obj_top = 0;
+//  let xpos;
+//  let ypos;
 
-  while (obj.offsetParent) {
-    obj_left += obj.offsetLeft;
-    obj_top += obj.offsetTop;
-    obj = obj.offsetParent;
-  }
-  if (mouseEvent) {
-    xpos = mouseEvent.pageX;
-    ypos = mouseEvent.pageY;
-  } else {
-    xpos = window.event.x + document.body.scrollLeft - 2;
-    ypos = window.event.y + document.body.scrollTop - 2;
-  }
-  xpos -= obj_left;
-  ypos -= obj_top;
+//  while (obj.offsetParent) {
+//    obj_left += obj.offsetLeft;
+//    obj_top += obj.offsetTop;
+//    obj = obj.offsetParent;
+//  }
+//  if (mouseEvent) {
+//    xpos = mouseEvent.pageX;
+//    ypos = mouseEvent.pageY;
+//  } else {
+//    xpos = window.event.x + document.body.scrollLeft - 2;
+//    ypos = window.event.y + document.body.scrollTop - 2;
+//  }
+//  xpos -= obj_left;
+//  ypos -= obj_top;
 
-  const imgWidth = imgElm.clientWidth;
-  const imgHeight = imgElm.clientHeight;
+//  const imgWidth = imgElm.clientWidth;
+//  const imgHeight = imgElm.clientHeight;
 
-  imgElm.style.top =
-    -(((imgHeight - this.clientHeight) * ypos) / this.clientHeight) + "px";
-  imgElm.style.left =
-    -(((imgWidth - this.clientWidth) * xpos) / this.clientWidth) + "px";
-});
+//  imgElm.style.top =
+//    -(((imgHeight - this.clientHeight) * ypos) / this.clientHeight) + "px";
+//  imgElm.style.left =
+//    -(((imgWidth - this.clientWidth) * xpos) / this.clientWidth) + "px";
+//});
 
-// change the current image
-Array.from(listProductElm.children).forEach((productElm, i, list) => {
-  productElm.addEventListener("click", function () {
-    const newSrc = productElm.querySelector("img").src;
-    imgElm.src = newSrc;
+//// change the current image
+//Array.from(listProductElm.children).forEach((productElm, i, list) => {
+//  productElm.addEventListener("click", function () {
+//    const newSrc = productElm.querySelector("img").src;
+//    imgElm.src = newSrc;
 
-    list.forEach((prod) => prod.classList.remove("active"));
-    productElm.classList.add("active");
-  });
-});
+//    list.forEach((prod) => prod.classList.remove("active"));
+//    productElm.classList.add("active");
+//  });
+//});
 
-//
+////
 
-function changeHeight() {
-  imgContElm.style.height = imgContElm.clientWidth + "px";
-}
-changeHeight();
+//function changeHeight() {
+//  imgContElm.style.height = imgContElm.clientWidth + "px";
+//}
+//changeHeight();
 
-// function changeWidth() {
-//   imgContElm.style.width=imgContElm.clientHeight+'px'
-// }
-// changeWidth()
+//// function changeWidth() {
+////   imgContElm.style.width=imgContElm.clientHeight+'px'
+//// }
+//// changeWidth()
 
-window.addEventListener("resize", changeHeight);
+//window.addEventListener("resize", changeHeight);
 
 // -----------------------------Like btn------------------------------
 
